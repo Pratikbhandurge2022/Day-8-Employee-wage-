c4cf2e9412dbaa625dde7eb029d31babfecaf26b
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-21 00:36:31 +0530

    uc2

---
+++ empwage/empwage/uc2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empwage
+{
+    internal class EmployeeAttendance
+    {
+        static int WagePerHour = 20;
+
+        public void getAttendance()
+        {
+            Random random = new Random();
+            int number = random.Next(0, 2);
+            CheckAttendance(number);
+        }
+
+        public static void CheckAttendance(int number)
+        {
+            int hours;
+            if (number == 0)
+            {
+                Console.WriteLine("employee is absent");
+                hours = 0;
+                CalculateSalary(hours);
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+                hours = 8;
+                CalculateSalary(hours);
+            }
+        }
+
+        public static void CalculateSalary(int hours)
+        {
+            int salary = WagePerHour * hours;
+            Console.WriteLine("Salary is " + salary);
+        }
+    }
+}
