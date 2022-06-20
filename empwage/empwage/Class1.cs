b6dd5d56f2670a74dd2b1be1a2682421bcef03d6
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-21 00:33:39 +0530

    uc1

---
+++ empwage/empwage/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    internal class EmployeeAttendance
+    {
+        public void getAttendance()
+        {
+            Random random = new Random();
+            int number = random.Next(0, 2);
+            CheckAttendance(number);
+        }
+
+        public static void CheckAttendance(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+            }
+        }
+    }
+}
---
+++ empwage/empwage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeAttendance attendance = new EmployeeAttendance();
+            attendance.getAttendance();
+        }
+    }
+}
